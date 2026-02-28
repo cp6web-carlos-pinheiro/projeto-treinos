fe824955f1258f0dfd9e19e93b6a3e55330a0504
devcrerrm122 <42558201-devcrerrm122@users.noreply.replit.com>
2026-02-28 22:45:39 +0000

    feat: add prisma and fatify swagger setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import "dotenv/config";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -13,6 +16,27 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para o bootcamp de treinos do FSC",
+      version: "1.0.0",
+    },
+    // servers: [
+    //   {
+    //     description: "Localhost",
+    //     url: "https://b0c66646-a2e4-4037-9f6f-fd0222b6ebba-00-19e4fhe7avt72.picard.replit.dev/",
+    //   },
+    // ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
@@ -28,13 +52,16 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
   handler: () => {
     return {
-      message: "Hello World 2",
+      message: "Hello World 4",
     };
   },
 });
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 5000 });
+  await app.listen({
+    port: Number(process.env.PORT) || 5000,
+    host: "0.0.0.0",
+  });
 } catch (err) {
   app.log.error(err);
   process.exit(1);

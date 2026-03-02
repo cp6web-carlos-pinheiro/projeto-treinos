b26dd42170cb8b423abb41f167cc41336dde58cf
devcrerrm122 <42558201-devcrerrm122@users.noreply.replit.com>
2026-03-02 21:05:14 +0000

    feat: add create workout plan endpoint

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,18 +1,12 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { openAPI } from "better-auth/plugins";
-
-import { PrismaClient } from "../generated/prisma/client.js";
-
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
-});
+import { prisma } from "./db.js";
 
 export const auth = betterAuth({
   trustedOrigins: [
     "http://localhost:3000",
-    "https://b0c66646-a2e4-4037-9f6f-fd0222b6ebba-00-19e4fhe7avt72.picard.replit.dev/", 
+    "https://b0c66646-a2e4-4037-9f6f-fd0222b6ebba-00-19e4fhe7avt72.picard.replit.dev/",
   ],
   emailAndPassword: {
     enabled: true,

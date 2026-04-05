317079e144e447a504e0a052ca27b3e5ca9c2745
devcrerrm122 <42558201-devcrerrm122@users.noreply.replit.com>
2026-04-05 03:15:25 +0000

    add web aplication

--- web/.next/types/app/auth/page.ts
+++ web/.next/types/app/auth/page.ts
@@ -1,8 +1,8 @@
-// File: /home/runner/workspace/web/app/workout-plans/[id]/page.tsx
-import * as entry from '../../../../../app/workout-plans/[id]/page.js'
+// File: /home/runner/workspace/web/app/auth/page.tsx
+import * as entry from '../../../../app/auth/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../app/workout-plans/[id]/page.js')
+type TEntry = typeof import('../../../../app/auth/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
